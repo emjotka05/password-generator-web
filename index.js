c9753d542ce84d7cb54669797fa0f4ce62eb6621
Max <emjotka05@gmail.com>
2026-03-14 13:08:48 +0100

    feat: Add javascript file preenting logic for generating passwords. Add transition to generate button.

---
+++ index.js
@@ -0,0 +1,45 @@
+const characters = [
+    "A","B","C","D",
+    "E","F","G","H",
+    "I","J","K","L",
+    "M","N","O","P",
+    "Q","R","S","T",
+    "U","V","W","X",
+    "Y","Z","a","b",
+    "c","d","e","f",
+    "g","h","i","j",
+    "k","l","m","n",
+    "o","p","q","r",
+    "s","t","u","v",
+    "w","x","y","z",
+    "0","1","2","3",
+    "4","5","6","7",
+    "8","9","~","`",
+    "!","@","#","$",
+    "%","^","&","*",
+    "(",")","_","-",
+    "+","=","{","[",
+    "}","]",",","|",
+    ":",";","<",">",
+    ".","?","/"];
+
+let generateBtnEl = document.getElementById("generate-btn")
+let passwordOneEl = document.getElementById("passwordOne")
+let passwordTwoEl = document.getElementById("passwordTwo")
+
+generateBtnEl.addEventListener("click", generatePasswords)
+
+function generatePasswords() {
+    let password_one = ""
+    let password_two = ""
+    for(let i=0;i<15;i++){
+        password_one += characters[getRandomNum()]
+        password_two += characters[getRandomNum()]
+    }
+    passwordOneEl.textContent = password_one
+    passwordTwoEl.textContent = password_two
+}
+
+function getRandomNum(){
+    return Math.floor(Math.random() * characters.length)
+}
